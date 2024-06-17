fc4bdf54413b079fdbedfe9d2f803e1f5d0aa948
gominnam <kosok03@naver.com>
2024-06-17 21:53:44 +0900

    add photo_provider and edit spot_provider

---
+++ lib/providers/photo_provider.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/foundation.dart';
+import 'package:odyssey_flutter_app/models/photo.dart';
+
+class PhotoProvide with ChangeNotifier {
+  final List<Photo> _photos = [];
+  List<Photo> get photos => _photos;
+
+  void addPhoto(Photo photo) {
+    _photos.add(photo);
+    notifyListeners();
+  }
+}
--- lib/providers/spot_provider.dart
+++ lib/providers/spot_provider.dart
@@ -1,23 +1,27 @@
 // lib/providers/spot_provider.dart
 
+import 'dart:io';
+
 import 'package:flutter/foundation.dart';
-import 'package:flutter_naver_map/flutter_naver_map.dart';
+import 'package:odyssey_flutter_app/models/spot.dart';
+import 'package:odyssey_flutter_app/models/photo.dart';
+// import 'package:flutter_naver_map/flutter_naver_map.dart';
 
-class Spot {
-  final String id;
-  final NLatLng position;
-  String? memo;
-  List<String>? photos;
+// class Spot {
+//   final String id;
+//   final NLatLng position;
+//   String? memo;
+//   List<String>? photos;
 
-  Spot({
-    required this.id, 
-    required this.position, 
-    this.memo,
-    this.photos,
-  }){
-    photos ??= [];
-  }
-}
+//   Spot({
+//     required this.id, 
+//     required this.position, 
+//     this.memo,
+//     this.photos,
+//   }){
+//     photos ??= [];
+//   }
+// }
 
 class SpotProvider with ChangeNotifier {
   
@@ -46,15 +50,37 @@ class SpotProvider with ChangeNotifier {
     notifyListeners();
   }
 
-  void addPhoto(String id, String photo) {
+  void addPhoto(String id, Photo photo) {
     final spot = _spots.firstWhere((spot) => spot.id == id);
     spot.photos!.add(photo);
     notifyListeners();
   }
 
-  void removePhoto(String id, String photo) {
+  void removePhoto(String id, Photo photo) {
     final spot = _spots.firstWhere((spot) => spot.id == id);
     spot.photos!.remove(photo);
     notifyListeners();
+  }
+
+  Spot getSpot(String id) {
+    return _spots.firstWhere((spot) => spot.id == id);
+  }
+
+  List<Spot> getAllSpots() {
+    return _spots;
+  }
+
+  List<Photo> getAllPhotos() {
+    final photos = <Photo>[];
+    for (final spot in _spots) {
+      photos.addAll(spot.photos!);
+    }
+    return photos;
+  }
+
+  File? getPhotoFile(String id, int index) {
+    final spot = _spots.firstWhere((spot) => spot.id == id);
+    final photo = spot.photos![index];
+    return File(photo.path);
   }
 }
